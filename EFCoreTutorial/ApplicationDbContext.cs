33513e17f0641d4de395063207ddad75290fb47d
hptdev <62223472+hpt-dev@users.noreply.github.com>
2022-09-07 00:06:23 +0100

    Add files via upload

---
+++ EFCoreTutorial/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EFCoreTutorial.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreTutorial
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        
+        public DbSet<User> Users { get; set; }
+
+    }
+}
